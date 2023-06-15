a92c76953d881f2be2062d0c4c73e2c8dd657f5c
ssp <sungsu930@naver.com>
2023-06-15 18:06:12 +0900

    성수 커밋 2

--- Test_Project/src/main/java/com/web/root/Test.java
+++ Test_Project/src/main/java/com/web/root/Test.java
@@ -3,5 +3,6 @@ package com.web.root;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("test_ sungsu");
+		System.out.println("test2_sungsu");
 	}
 }
---
+++ Test_Project/src/main/java/com/web/root/Test2.java
@@ -0,0 +1,11 @@
+package com.web.root;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("git 힘듬");
+		// TODO Auto-generated method stub
+
+	}
+
+}
